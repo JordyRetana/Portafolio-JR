e4c46022777c3278065b8cb260494aa6acbbf2da
Jordy Retana <jretanamendez@gmail.com>
2026-04-10 13:32:12 -0600

    Fix GitHub Pages 404 with HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 import { LanguageProvider } from './i18n/LanguageContext'
 import { ThemeProvider } from './theme/ThemeContext'
@@ -8,13 +8,13 @@ import ScrollToTop from './components/common/ScrollToTop'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Portafolio-JR">
+    <HashRouter>
       <LanguageProvider>
         <ThemeProvider>
           <ScrollToTop />
           <App />
         </ThemeProvider>
       </LanguageProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
