5d85401a15b680a0da01563e3f96e6859d172c7e
Jordy Retana <jretanamendez@gmail.com>
2026-04-09 22:17:47 -0600

    Conectar frontend con backend en Render

--- src/services/contactService.js
+++ src/services/contactService.js
@@ -1,5 +1,5 @@
 export const sendContactMessage = async (payload) => {
-  const response = await fetch('http://localhost:4000/api/contact', {
+  const response = await fetch('https://portafolio-jr-backend.onrender.com/api/contact', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
